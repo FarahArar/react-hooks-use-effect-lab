fd3899cf64a512b9eb3e7378f25ca3638612da92
Farah Arar <farah02arar@gmail.com>
2023-10-30 14:40:57 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Question from "./Question";
 import quiz from "../data/quiz";
 
-function App() {
+function App({ onAnswered }) {
   const [questions, setQuestions] = useState(quiz);
   const [currentQuestionId, setCurrentQuestion] = useState(1);
   const [score, setScore] = useState(0);
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false); 
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleQuestionAnswered(correct) {
-    if (currentQuestionId < questions.length) {
+    if (currentQuestionId < questions.length) { // Change this line
       setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
     } else {
       setCurrentQuestion(null);
@@ -22,6 +40,7 @@ function App() {
   return (
     <main>
       <section>
+        <p>Time Remaining: {timeRemaining} seconds</p>
         {currentQuestion ? (
           <Question
             question={currentQuestion}
